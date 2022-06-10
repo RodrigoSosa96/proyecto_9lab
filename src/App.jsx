beef3965e07d4d5bfc27d52afa0b97642b95d76d
Rodrigo Sosa <rod2008@live.com.ar>
2022-06-10 13:15:23 -0300

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/components/**/*.{html,jsx}",
+    "./src/routes/**/*.{html,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
