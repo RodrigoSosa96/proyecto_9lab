16bc0044196d5a8f9ad0e562a40992371425e839
RodrigoSosa96 <rod2008@live.com.ar>
2022-09-17 21:46:23 -0300

    fix: define env v2

--- src/api/api.js
+++ src/api/api.js
@@ -4,8 +4,7 @@ export const openlibraryAPI = axios.create({
   baseURL: 'https://openlibrary.org/api/',
 })
 
-export const BASE_URL = import.meta.env.WORKER_API_URL || 'http://localhost:8787/'
-
+export const BASE_URL = import.meta.env.VITE_WORKER_API_URL || 'http://localhost:8787/'
 export default axios.create({
   baseURL: BASE_URL
 });
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command, mode }) => {
 
-  const env = loadEnv(mode, process.cwd(), '')
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
   return {
-    define: {
-      WORKER_API_URL: env.WORKER_API_URL
-    },
     plugins: [react()],
   }
 })
